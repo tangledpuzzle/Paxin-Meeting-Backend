4c03bd3dbf4b07cf24ed8cb583d3e4e3f79308e6
tangledpuzzle <tangledpuzzle94621@gmail.com>
2023-11-15 23:20:37 +0400

    main cors changes

--- cmd/main.go
+++ cmd/main.go
@@ -117,7 +117,7 @@ func main() {
 
 	app.Use(cors.New(cors.Config{
 		AllowOrigins:     "*",
-		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization, Access-Control-Allow-Headers",
 		AllowMethods:     "GET, POST, PATCH, DELETE",
 		AllowCredentials: true,
 	}))
