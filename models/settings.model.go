bb695b19da13a102a78dfcc2f7a579c03dbf7b0e
tangledpuzzle <tangledpuzzle94621@gmail.com>
2023-11-18 19:49:54 +0400

    langs settings 2

--- models/settings.model.go
+++ models/settings.model.go
@@ -7,6 +7,7 @@ import (
 type Langs struct {
 	ID        uint       `gorm:"primary_key"`
 	Name      string     `gorm:"not null"`
+	Code      string     `gorm:"not null"`
 	UpdatedAt time.Time  `gorm:"not null"`
 	DeletedAt *time.Time `gorm:"index"`
 }
