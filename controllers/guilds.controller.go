2f087591e08b714b95240db5869657672dc390f1
tangledpuzzle <tangledpuzzle94621@gmail.com>
2023-11-27 17:10:19 +0400

    fix get guild name

--- controllers/guilds.controller.go
+++ controllers/guilds.controller.go
@@ -24,38 +24,32 @@ type GuildResponse struct {
 func GetGuildName(c *fiber.Ctx) error {
 	name := c.Query("name")
 	lang := c.Query("lang")
-	mode := c.Query("mode")
-	if mode == "translate" {
-		var guildTranslation models.GuildTranslation
-		if err := initializers.DB.
-			Where("name ILIKE ?", "%"+name+"%").
-			First(&guildTranslation).Error; err != nil {
-			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-				"status":  "error",
-				"message": "Failed to fetch guild translation from the database",
-			})
-		}
 
-		var translatedGuild models.GuildTranslation
-		if err := initializers.DB.
-			Where("guild_id = ? AND language = ?", guildTranslation.GuildID, lang). // Replace "EN" with your target language code
-			First(&translatedGuild).Error; err != nil {
-			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-				"status":  "error",
-				"message": "Failed to fetch translated guild name from the database",
-			})
-		}
-
-		return c.JSON(fiber.Map{
-			"status": "success",
-			"data":   translatedGuild.Name,
+	var guildTranslation models.GuildTranslation
+	if err := initializers.DB.
+		Where("name ILIKE ?", "%"+name+"%").
+		First(&guildTranslation).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"status":  "error",
+			"message": "Failed to fetch guild translation from the database",
 		})
-
 	}
+
+	var translatedGuild models.GuildTranslation
+	if err := initializers.DB.
+		Where("guild_id = ? AND language = ?", guildTranslation.GuildID, lang). // Replace "EN" with your target language code
+		First(&translatedGuild).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"status":  "error",
+			"message": "Failed to fetch translated guild name from the database",
+		})
+	}
+
 	return c.JSON(fiber.Map{
 		"status": "success",
-		"data":   "",
+		"data":   translatedGuild.Name,
 	})
+
 }
 
 func GetGuilds(c *fiber.Ctx) error {
