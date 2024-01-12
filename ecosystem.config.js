918837d5a1b9a08d86e91ab507a310a12671b426
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-01-12 05:51:03 +0000

    fix: pm2 config, docker compose

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,10 @@ module.exports = {
     apps: [
         {
             name: 'paxintrade-api',
-            script: '/app/main',
             exec_mode: 'cluster',
             instances: 'max', // Or a number of instances
+            autorestart: true,
+            script: '/app/main',
         }
     ]
 }
