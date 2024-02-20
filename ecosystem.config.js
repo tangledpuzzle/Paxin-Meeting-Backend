c9c00806584890b7572772565a7fe78d2f488316
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-02-20 15:34:14 +0800

    fix: ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,11 @@ module.exports = {
       exec_mode: "cluster",
       instances: 2, // 'max' or a number of instances
       autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      restart_delay: 5000,
+      max_restarts: 10,
+      min_uptime: "10s",
       script: "/app/paxintrade-api",
     },
   ],
