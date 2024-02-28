0f6b9afe9ffd69df0b77c03ab142e630cfc5d2d6
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-02-28 11:52:11 +0800

    fix: file name

--- models/hashtags.model.go
+++ models/hashtags.model.go
@@ -10,3 +10,10 @@ type Hashtags struct {
 	UpdatedAt time.Time  `gorm:"not null"`
 	DeletedAt *time.Time `gorm:"index"`
 }
+
+type HashtagsForProfile struct {
+	ID        uint       `gorm:"primary_key"`
+	Hashtag   string     `gorm:"not null;unique"`
+	UpdatedAt time.Time  `gorm:"not null"`
+	DeletedAt *time.Time `gorm:"index"`
+}
