0c6280c4efb5ba586425741775ab52600733d280
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-03-11 15:53:04 -0400

    presaved filters remove commit

--- controllers/presavedfilter.controller.go
+++ controllers/presavedfilter.controller.go
@@ -8,29 +8,23 @@ import (
 )
 
 func CreatePresavedfilter(c *fiber.Ctx) error {
-	// Получаем аутентифицированного пользователя из контекста (предположим, что он находится в локальных данных)
 	user := c.Locals("user").(models.UserResponse)
 
-	// Парсим JSON тело запроса в структуру Presavedfilters
 	filter := new(models.Presavedfilters)
 	if err := c.BodyParser(filter); err != nil {
 		return err
 	}
 
-	// Устанавливаем UserID для фильтра
 	filter.UserID = user.ID
 
-	// Сохраняем фильтр в базе данных
 	result := initializers.DB.Create(filter)
 	if result.Error != nil {
-		// В случае ошибки сохранения возвращаем ошибку 500
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"status":  "error",
 			"message": "Failed to create presaved filter",
 		})
 	}
 
-	// Возвращаем успешный ответ
 	return c.JSON(fiber.Map{
 		"status":  "success",
 		"message": "Presaved filter created successfully",
