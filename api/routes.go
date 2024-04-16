55f9b20b77a942aec07849a30f52ecc7f876bfe7
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-04-16 15:32:50 -0400

    remove checkrole when create new request

--- api/routes.go
+++ api/routes.go
@@ -38,7 +38,7 @@ func Register(micro *fiber.App) {
 	})
 
 	micro.Route("/newreq", func(router fiber.Router) {
-		router.Post("/post", middleware.CheckRole([]string{"admin", "user", "vip"}), controllers.Userq)
+		router.Post("/post", controllers.Userq)
 	})
 
 	micro.Route("/auth", func(router fiber.Router) {
