d10f0c2148f629931c509757eff05d65cb9b8eab
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-05-23 18:48:49 +0000

    deploy_release-prod.sh edited online with Bitbucket

--- deploy_release-prod.sh
+++ deploy_release-prod.sh
@@ -30,6 +30,7 @@ ssh -o StrictHostKeyChecking=no -i "$PRIVATE_KEY_PATH" "$HOST_ADDRESS" << ENDSSH
   echo $ECR_PASSWORD | docker login --username AWS --password-stdin $REPOSITORY_URI
   git restore .
   git pull
+  sed -i.bak "s|image: paxintrade-api:latest-prod|image: $REPOSITORY_URI/paxintrade-api:latest-prod|g" docker-compose.yml
   docker compose stop $SERVICE_NAME || { echo "Failed to stop $SERVICE_NAME"; exit 1; }
   docker compose pull $SERVICE_NAME || { echo "Failed to pull $SERVICE_NAME"; exit 1; }
   docker compose up -d || { echo "Failed to start services with Docker Compose."; exit 1; }
