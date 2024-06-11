cf5a5c53632eeaf5dd9c4668e9e4f204c9b24730
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-06-11 17:52:11 +0000

    deploy_release-prod.sh edited online with Bitbucket

--- deploy_release-prod.sh
+++ deploy_release-prod.sh
@@ -30,6 +30,7 @@ ssh -o StrictHostKeyChecking=no -i "$PRIVATE_KEY_PATH" "$HOST_ADDRESS" << ENDSSH
   echo $ECR_PASSWORD | docker login --username AWS --password-stdin $REPOSITORY_URI
   git restore .
   git pull
+  cd ~/workspace/myru/mainsite/backend
   sed -i.bak "s|image: myru-api:latest-prod|image: $REPOSITORY_URI/myru-api:latest-prod|g" docker-compose.yml
   docker compose stop $SERVICE_NAME || { echo "Failed to stop $SERVICE_NAME"; exit 1; }
   docker compose pull $SERVICE_NAME || { echo "Failed to pull $SERVICE_NAME"; exit 1; }
