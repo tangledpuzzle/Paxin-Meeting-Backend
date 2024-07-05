6d185f68d5fc327616f4836e1a85da37b194bc13
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-07-05 17:29:27 +0400

    add msg for donat 2

--- utils/ws.go
+++ utils/ws.go
@@ -110,6 +110,12 @@ func SendPersonalMessageToClient(clientID, message string) error {
 		}
 	}
 
+	if message == "newDonat" {
+		if err := conn.WriteMessage(websocket.TextMessage, jsonData); err != nil {
+			return err
+		}
+	}
+
 	if message == "newblog" {
 
 		// Get the total count of records in the "blog" table
