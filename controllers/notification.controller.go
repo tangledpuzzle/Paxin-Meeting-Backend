74422bd9f317b4d891d616b980e24d47bba67239
tangledpuzzle <tangledpuzzle94621@gmail.com>
2024-07-15 13:20:39 +0400

    add controller for notification

--- controllers/notification.controller.go
+++ controllers/notification.controller.go
@@ -1,1 +1,22 @@
 package controllers
+
+import (
+	"hyperpage/initializers"
+	"hyperpage/models"
+	"hyperpage/utils"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func GetNotifications(c *fiber.Ctx) error {
+	user := c.Locals("user").(models.UserResponse)
+
+	var notifications []models.Notification
+	db := initializers.DB.Where("user_id = ?", user.ID)
+
+	if err := utils.Paginate(c, db, &notifications); err != nil {
+		return err
+	}
+
+	return nil
+}
