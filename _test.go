edc4860b0e811b662ba7a66ebc2c2c20c252ecff
TangledPuzzle <132536190+tangledpuzzle@users.noreply.github.com>
2024-10-11 19:54:47 +0300

    Update _test.go

--- _test.go
+++ _test.go
@@ -1,1 +1,14 @@
+package routes
 
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func MainView(app *fiber.App) {
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("index", fiber.Map{
+			"Title":       "Powerful Paxintrade API Server",
+			"Description": "Server is developed by paxintrade",
+		})
+	})
+}
